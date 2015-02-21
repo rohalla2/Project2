d08b5c2ef3787b3c0dff8e2521b8f13654b359ef
Jeff Rohalla <jrohalla@uchicago.edu>
2015-02-21 14:29:48 -0600

    Add flag requrements for SSL server

--- src/Driver.java
+++ src/Driver.java
@@ -7,20 +7,30 @@ public class Driver {
     public static void main(String[] argv) {
         Map<String, String> flags = Utils.parseCmdlineFlags(argv);
         if (!flags.containsKey("--serverPort")) {
-            System.out.println("usage: Server --serverPort=12345");
+            System.out.println("usage: Driver --serverPort=12345 --sslServerPort=23456");
+            System.exit(-1);
+        }
+
+        if (!flags.containsKey("--sslServerPort")) {
+            System.out.println("useage: Driver --serverPort=12345 --sslServerPort=23456");
             System.exit(-1);
         }
 
         int serverPort = -1;
+        int sslServerPort = -1;
+
         try {
             serverPort = Integer.parseInt(flags.get("--serverPort"));
+            sslServerPort = Integer.parseInt(flags.get("--sslServerPort"));
         } catch (NumberFormatException e) {
             System.out.println("Invalid port number! Must be an integer.");
             System.exit(-1);
         }
 
-        HTTPServer server1 = new HTTPServer(serverPort);
-        server1.start();
+        HTTPServer httpServer = new HTTPServer(serverPort);
+        //SecureHTTPServer sslServer = new SecureHTTPServer(sslServerPort);
+        httpServer.start();
+        //sslServer.start();
 
     }
 }
