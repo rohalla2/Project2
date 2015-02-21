1d6f1f9b9d82173482e4ec9b0d5c9ee787fb95a4
Jeff Rohalla <jrohalla@uchicago.edu>
2015-02-21 16:56:39 -0600

    Partial Server work

--- src/SecureHTTPServer.java
+++ src/SecureHTTPServer.java
@@ -1,34 +1,64 @@
-import javax.xml.bind.SchemaOutputResolver;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.net.ssl.*;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
-import java.util.Map;
 
 /**
  * Created by rohallaj on 2/20/15.
  */
 public class SecureHTTPServer extends AbstractServer {
     private final int SOCKET_LIVE_TIME = 5;
-    private ServerSocket socket;
+    private SSLServerSocket socket;
     private Socket mClientSocket;
-    public ServerSocket getSocket() {
-        return socket;
-    }
+    private KeyStore mKeyStore;
+    //private SSLServerSocket mSSLServerSocket;
+    private SSLContext mSSLContext;
+    private KeyManagerFactory mKeyManagerFactory;
+    private SSLServerSocketFactory mSSLServerSocketFactory;
 
-    public void setSocket(ServerSocket socket) {
-        this.socket = socket;
-    }
+    // http://www.java2s.com/Tutorial/Java/0490__Security/KeyStoreExample.htm
+    // http://www.programcreek.com/java-api-examples/index.php?api=javax.net.ssl.KeyManagerFactory
+    // http://www.herongyang.com/JDK/SSL-Socket-Server-Example-SslReverseEchoer.html
+    public SecureHTTPServer(int serverPort){
+        super(serverPort);
+        try {
+            mKeyStore = KeyStore.getInstance("JKS");
+            char[] password = "password1".toCharArray();
+            FileInputStream fIn = new FileInputStream("server.jks");
+            mKeyStore.load(fIn,password);
 
-    public Socket getClientSocket() {
-        return mClientSocket;
-    }
+            mKeyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+            mKeyManagerFactory.init(mKeyStore,password);
 
-    public void setClientSocket(Socket clientSocket) {
-        mClientSocket = clientSocket;
+            mSSLContext = SSLContext.getInstance("TLS");
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(mKeyStore);
+            mSSLContext.init(mKeyManagerFactory.getKeyManagers(),tmf.getTrustManagers(),new SecureRandom());
+            mSSLServerSocketFactory = mSSLContext.getServerSocketFactory();
+            socket = (SSLServerSocket) mSSLServerSocketFactory.createServerSocket(getServerPort());
+
+            printServerSocketInfo(socket);
+
+            System.out.println("SSL SERVER STARTED ON PORT " + getServerPort());
+
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -42,6 +72,7 @@ public class SecureHTTPServer extends AbstractServer {
                 setLeaveConnectionOpen(true);
                 if (acceptFromClient()) {
                     System.out.println("----- NEW CLIENT CONNECTION ESTABLISHED -----");
+                    printServerSocketInfo(socket);
                     int waitInterval = 0;
                     while (getLeaveConnectionOpen()) {
                         boolean isEmpty = false;
@@ -52,6 +83,7 @@ public class SecureHTTPServer extends AbstractServer {
                             }
 //                            System.out.println("before get request header");
                             ArrayList<String> requestHeader = getRequestHeader();
+                            System.out.println(requestHeader.get(0));
 
                             if(requestHeader != null && !requestHeader.isEmpty() && ( requestHeader.contains("Connection: close\r\n") || requestHeader.get(0).contains("HTTP/1.0"))) {
 //                                System.out.println("In check for close");
@@ -93,25 +125,20 @@ public class SecureHTTPServer extends AbstractServer {
         }
     }
 
+    public void initializeSSL(){
 
-    public SecureHTTPServer(int serverPort){
-        super(serverPort);
     }
 
     @Override
     public void bind()  {
-        try{
-            setSocket(new ServerSocket(getServerPort()));
-            System.out.println("HTTP Server bound and listening to port " + getServerPort());
-        } catch (IOException e) {
-            System.out.println("Error binding to port " + getServerPort());
-        }
+        //socket = (mSSLServerSocket);
+        System.out.println("HTTP Server bound and listening to port " + getServerPort());
     }
 
     @Override
     public boolean acceptFromClient() throws IOException {
         try {
-            setClientSocket(getSocket().accept());
+           mClientSocket =  ((Socket) socket.accept());
         } catch (SecurityException e) {
             System.out.println("The security manager intervened; your config is very wrong. " + e);
             return false;
@@ -120,8 +147,8 @@ public class SecureHTTPServer extends AbstractServer {
             return false;
         }
 
-        setToClientStream(new DataOutputStream(getClientSocket().getOutputStream()));
-        setFromClientStream(new BufferedReader(new InputStreamReader(getClientSocket().getInputStream())));
+        setToClientStream(new DataOutputStream(mClientSocket.getOutputStream()));
+        setFromClientStream(new BufferedReader(new InputStreamReader(mClientSocket.getInputStream())));
         return true;
     }
 
@@ -133,5 +160,35 @@ public class SecureHTTPServer extends AbstractServer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void printSocketInfo(SSLSocket s) {
+        System.out.println("Socket class: "+s.getClass());
+        System.out.println("   Remote address = "
+                +s.getInetAddress().toString());
+        System.out.println("   Remote port = "+s.getPort());
+        System.out.println("   Local socket address = "
+                +s.getLocalSocketAddress().toString());
+        System.out.println("   Local address = "
+                +s.getLocalAddress().toString());
+        System.out.println("   Local port = "+s.getLocalPort());
+        System.out.println("   Need client authentication = "
+                +s.getNeedClientAuth());
+        SSLSession ss = s.getSession();
+        System.out.println("   Cipher suite = "+ss.getCipherSuite());
+        System.out.println("   Protocol = "+ss.getProtocol());
+    }
+    private static void printServerSocketInfo(SSLServerSocket s) {
+        System.out.println("Server socket class: "+s.getClass());
+        System.out.println("   Socker address = "
+                +s.getInetAddress().toString());
+        System.out.println("   Socker port = "
+                +s.getLocalPort());
+        System.out.println("   Need client authentication = "
+                +s.getNeedClientAuth());
+        System.out.println("   Want client authentication = "
+                +s.getWantClientAuth());
+        System.out.println("   Use client mode = "
+                +s.getUseClientMode());
     }
 }
