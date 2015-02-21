9dac401590758177f69139e8594c7932ed1cb2bc
Jeff Rohalla <jrohalla@uchicago.edu>
2015-02-21 11:59:33 -0600

    Not quite working persistance

--- src/HTTPServer.java
+++ src/HTTPServer.java
@@ -1,3 +1,4 @@
+import javax.xml.bind.SchemaOutputResolver;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -10,7 +11,7 @@ import java.util.Map;
  * Created by rohallaj on 2/20/15.
  */
 public class HTTPServer extends AbstractServer {
-
+    private final int SOCKET_LIVE_TIME = 5;
     @Override
     public void run() {
         super.run();
@@ -20,41 +21,54 @@ public class HTTPServer extends AbstractServer {
         while(true) {
             try {
                 setLeaveConnectionOpen(true);
-                    if (acceptFromClient()) {
+                if (acceptFromClient()) {
+                    System.out.println("----- NEW CLIENT CONNECTION ESTABLISHED -----");
                     while (getLeaveConnectionOpen()) {
                         int waitInterval = 0;
                         while(!getFromClientStream().ready()){
                             Thread.sleep(1000);
                             waitInterval++;
                             System.out.println("in wait loop " + waitInterval);
-                            if(waitInterval == 5){
+                            if(waitInterval == SOCKET_LIVE_TIME){
                                 setLeaveConnectionOpen(false);
                                 break;
                             }
                         }
+                        System.out.println("Outside of wait loop");
+                        if (waitInterval == SOCKET_LIVE_TIME) {
+                            break;
+                        }
                         ArrayList<String> requestHeader = getRequestHeader();
+                        System.out.println("Before Check header for close connection");
+                        if(requestHeader != null && !requestHeader.isEmpty() && ( requestHeader.contains("Connection: close\r\n") || requestHeader.get(0).contains("HTTP/1.0"))) {
+                            System.out.println("In check for close");
+                            setLeaveConnectionOpen(false);
+                        }
+                        System.out.println("After check");
+
+                        System.out.println("Before process header");
                         // split the first line of the request
                         if (requestHeader == null || requestHeader.isEmpty()) {
                             System.out.println("Ignoring empty request...");
                         } else {
+                            System.out.println("About to process request");
                             String[] requests = requestHeader.get(0).split(" ");
                             // process the request
                             processRequest(requests[0], requests[1]);
                         }
-                        if(requestHeader != null && !requestHeader.isEmpty() && ( requestHeader.contains("Connection: close\r\n") || requestHeader.get(0).contains("HTTP/1.0"))) {
-                            setLeaveConnectionOpen(false);
-                        }
                     }
-                    } else {
-                        System.out.println("Error accepting client connection.");
-                    }
-                }catch(IOException e){
-                    System.out.println("Error communicating with client. aborting. Details: " + e);
-                } catch (InterruptedException e){
-                    System.out.println(e);
-                    System.out.println(e);
+                    System.out.println(" ----- ENDED -----");
+                } else {
+                    System.out.println("Error accepting client connection.");
                 }
+            }catch(IOException e){
+                System.out.println("Error communicating with client. aborting. Details: " + e);
+            } catch (InterruptedException e){
+                System.out.println(e);
+                System.out.println(e);
+            }
             // close sockets and buffered readers
+            System.out.println("Cleaning UP");
             serverCleanup();
         }
     }
